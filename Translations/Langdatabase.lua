f951a5f86ce790c10e0264bdf2061caade13fef7
ONELua Team <oneluaproject@gmail.com>
2020-01-03 21:10:14 -0600

    Primera prueba
    revisando las funciones descargar idiomas para bajar spanish y actualización english_us

--- Translations/Langdatabase.lua
+++ Translations/Langdatabase.lua
@@ -1,3 +1,4 @@
 Online_Langs = {
-	{ id =  "ENGLISH_US",		version = "01.00",		author =  "gdljjrod" },
+{ id =  "ENGLISH_US",		version = "01.01",		author =  "gdljjrod" },
+{ id =  "SPANISH",			version = "01.00",		author =  "gdljjrod" },
 }
