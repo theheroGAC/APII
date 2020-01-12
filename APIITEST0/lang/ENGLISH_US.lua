86700517c8098d2d4c7e678a11002158f8b2e1f5
ONELua Team <oneluaproject@gmail.com>
2020-01-12 12:26:53 -0600

    Actualizado textos base

--- APIITEST0/lang/ENGLISH_US.lua
+++ APIITEST0/lang/ENGLISH_US.lua
@@ -55,13 +55,13 @@ ENGLISH_US["MENU_EXTRAS_CUSTOMWARNING_DESC"] =			"Allows you to set a custom war
 ENGLISH_US["MENU_EXTRAS_TRANSLATE_DESC"] =				"Help improve Autoplugin II by translating to your language!"
 
 --Options Settings
-ENGLISH_US["MENU_SETTINGS_LANG_ONLINE"] =				"Descargar Idiomas Disponibles"
+ENGLISH_US["MENU_SETTINGS_LANG_ONLINE"] =				"Download available languages"
 ENGLISH_US["MENU_SETTINGS_ENABLE_AUTOUPDATE"] =			"Enable AutoUpdate: "
-ENGLISH_US["MENU_SETTINGS_CHANGE_LANGUAGE"] =			"Cambiar Idioma"
+ENGLISH_US["MENU_SETTINGS_CHANGE_LANGUAGE"] =			"Change language"
 
-ENGLISH_US["MENU_SETTINGS_LANG_ONLINE_DESC"] =			"Descarga paquetes de idiomas disponibles para Autoplugin II"
-ENGLISH_US["MENU_SETTINGS_AUTOUPDATE_DESC"] =			"Habilitar AutoUpdate Si/No"
-ENGLISH_US["MENU_SETTINGS_CHANGE_LANGUAGE_DESC"] =		"Permite cambiar el idioma y queda seteado esta opción en cada inicio de AutoPlugin II"
+ENGLISH_US["MENU_SETTINGS_LANG_ONLINE_DESC"] =			"Download available language packs for Autoplugin II"
+ENGLISH_US["MENU_SETTINGS_AUTOUPDATE_DESC"] =			"Enable AutoUpdate Yes/No"
+ENGLISH_US["MENU_SETTINGS_CHANGE_LANGUAGE_DESC"] =		"Allows to change and set language as default for AutoPlugin II"
 
 --Install Plugins
 ENGLISH_US["LIST_PLUGINS"] =							"List of plugins ready to install:"
@@ -197,17 +197,17 @@ ENGLISH_US["MARK_LANGS"] =								"Mark/Unmark Lang(s)"
 ENGLISH_US["CLEAN_LANGS"] =								"To clean selected Langs"
 
 --Plugins Online
-ENGLISH_US["MENU_TITLE_PLUGINS_ONLINE"] =					"Actualización de Plugins"
+ENGLISH_US["MENU_TITLE_PLUGINS_ONLINE"] =				"Actualización de Plugins"
 
 --Langs
-ENGLISH_US["JAPANESE"]	=								"JAPANESE"
+ENGLISH_US["JAPANESE"] =								"JAPANESE"
 ENGLISH_US["ENGLISH_US"] =								"ENGLISH_US"
 ENGLISH_US["FRENCH"] =									"FRENCH"
 ENGLISH_US["SPANISH"] =									"ESPAÑOL"
 ENGLISH_US["GERMAN"] =									"GERMAN"
 ENGLISH_US["ITALIAN"] =									"ITALIAN"
 ENGLISH_US["DUTCH"]	=									"DUTCH"
-ENGLISH_US["PORTUGUESE"]=								"PORTUGUESE"
+ENGLISH_US["PORTUGUESE"] =								"PORTUGUESE"
 ENGLISH_US["RUSSIAN"] =									"RUSSIAN"
 ENGLISH_US["KOREAN"] =									"KOREAN"
 ENGLISH_US["CHINESE_T"] =								"CHINESE_T"
@@ -266,6 +266,7 @@ ENGLISH_US["INSTALLP_DESC_LOLITA4"] =					"Sets your Vita's clocks to 444 mhz al
 ENGLISH_US["INSTALLP_DESC_PSVSHELL"] =					"Yet another overclocking plugin"
 ENGLISH_US["INSTALLP_DESC_SYSCALL6"] =					"A tool that patches Playstation Vita's lv0's secure_kernel's syscall 6 (SELF/RVK checks)"
 ENGLISH_US["INSTALLP_DESC_MONAURAL"] =					"Mono audio and channel balance for Playstation Vita(requires vpk of the application)."
+ENGLISH_US["INSTALLP_DESC_MUPREMIUM"] =					"Enable background music play for any game or application."
 
 ENGLISH_US["INSTALLP_DESC_CUSTOMBOOTSPLASH"] =			"This plugin can display Boot Splash on PS Vita/PSTV at boot. Requires ENSO."
 ENGLISH_US["INSTALLP_DESC_CONVERTBOOTSPLASH"] =			"Set your splash image as custom boot splash ?"
@@ -288,6 +289,7 @@ ENGLISH_US["INSTALLP_DESC_AUTOBOOT"] =					"This plugin simply auto-launch an ap
 ENGLISH_US["INSTALLP_DESC_VITACOMPA"] =					"Plugin to ease the development of homebrews and plugins on the PS Vita"
 ENGLISH_US["INSTALLP_DESC_VOLUPROF"] =					"Automatically switch between your preferred speaker and headphone volumes."
 ENGLISH_US["INSTALLP_DESC_DOLCEPOLCE"] =				"Removes PSTV Blacklist Checks (VITA/PSP/PS1/PSM)"
+ENGLISH_US["INSTALLP_DESC_LOWMEMMODE"] =				"Launch system applications with enlarged memory game in the background"
 
 --ALL
 ENGLISH_US["INSTALLP_DESC_VSHMENU"] =					"A VSH menu taiHEN plugin for the PS VITA games and homebrews"
@@ -297,7 +299,6 @@ ENGLISH_US["INSTALLP_DESC_FRAMECOUNTER"] =				"This is a little plugin that work
 ENGLISH_US["INSTALLP_DESC_VGI"] =						"A simple taihen plugin that prints useful information about PS Vita games."
 ENGLISH_US["INSTALLP_DESC_FUCKPSSE"] =					"A plugin to decrypt PSM games"
 ENGLISH_US["INSTALLP_DESC_PSMPATCH"] =					"Allows you to modify PSM Games, and also can be used to load PSM Homebrew."
---ENGLISH_US["INSTALLP_DESC_ITLSENSO"] =				"Adds TLS v1.2 to Enso enabled devices(requires vpk of the application)."
 ENGLISH_US["INSTALLP_DESC_TROPHAX"] =					"Universal trophy unlocker for PSVITA."
 ENGLISH_US["INSTALLP_DESC_TURBOPAD"] =					"This plugin allows you to enable RapidFire on whatever button."
 ENGLISH_US["INSTALLP_DESC_RESCALER"] =					"This plugin replaces the original rescaler for PSVITA with a new one allowing games running at non-native resolution to get graphics improvements thanks to filtering with shaders."
@@ -310,11 +311,14 @@ ENGLISH_US["INSTALLP_DESC_REMAPSV"] =					"remaPSV is a remap plugin allowing yo
 ENGLISH_US["INSTALLP_DESC_FORCELANG"] =					"A taiHEN plugin for overriding an application's language"
 ENGLISH_US["INSTALLP_DESC_SHOWINFO"] =					"Simple plugin that shows title_id, stitle and app_ver of the running application."
 ENGLISH_US["INSTALLP_DESC_SHOWINFOFU"] =				"Simple plugin that shows title_id, stitle and app_ver of the running application.(full version)"
+ENGLISH_US["INSTALLP_DESC_MODALVOL"] =				    "This pack consits of applications to control system audio volume on PS vita in real time without closing games"
 
 --*NPXS10013
 ENGLISH_US["INSTALLP_DESC_PS4LINK"] =			        "taiHEN plugin that allows to force preferred Remote Play button configuration"
+
 --*NPXS10015 AND --*NPXS10016
 ENGLISH_US["INSTALLP_DESC_MACSPOOF"] =			        "A plugin to spoof your MAC address in the System Information"
+
 --*NPXS10036
 ENGLISH_US["INSTALLP_DESC_RESCREENY"] =			        "Vita screenshots will be saved into a folder named after the game."
 
