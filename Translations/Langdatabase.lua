46bad3b983c1ace4dfe42547c93d5b3efcb7e964
ONELua Team <oneluaproject@gmail.com>
2020-01-18 22:06:51 -0600

    debe ser lang online

--- Translations/Langdatabase.lua
+++ Translations/Langdatabase.lua
@@ -1,5 +1,5 @@
-Langs = {
-{ id =  "ENGLISH_US",	version = "01.00",		author =  "gdljjrod" },
-{ id =  "SPANISH",		version = "01.00",		author =  "gdljjrod" },
+Online_Langs = {
+{ id =  "ENGLISH_US",	version = "01.01",		author =  "gdljjrod" },
+{ id =  "SPANISH",		version = "01.01",		author =  "gdljjrod" },
 { id =  "ITALIAN",		version = "01.00",		author =  "TheHeroeGAC" },
 }
