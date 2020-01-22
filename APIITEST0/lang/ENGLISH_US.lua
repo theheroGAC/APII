4de9db42a85812ebaca80f428ca706f7eef3c58a
ONELua Team <oneluaproject@gmail.com>
2020-01-21 23:56:14 -0600

    3er update... idiomas van en lang

--- APIITEST0/lang/ENGLISH_US.lua
+++ APIITEST0/lang/ENGLISH_US.lua
@@ -86,6 +86,7 @@ ENGLISH_US["UNINSTALLP_TITLE"] =						"Plugins Manager"
 ENGLISH_US["UNINSTALLP_NOCONFIG_FOUND"] =				"No config.txt found at: "
 ENGLISH_US["UNINSTALLP_PLUGIN"] =						"To uninstall plugin"
 ENGLISH_US["UNINSTALLP_LEFTRIGHT_SECTION"] =			"To switch section"
+ENGLISH_US["UNINSTALLP_EMPTY"] =						"EMPTY !!!"
 
 --install sd2vita
 ENGLISH_US["MENU_TITLE_SD2VITA"] =						"Install Storage Manager plugin for SD2VITA"
@@ -219,6 +220,9 @@ ENGLISH_US["ENGLISH_GB"] =								"ENGLISH_GB"
 ENGLISH_US["TURKISH"] =									"TURKISH"
 
 --descriptions plugins
+--StorageMgr
+ENGLISH_US["INSTALLP_DESC_SD2VITA"] =					"Install Plugin for SD2VITA"
+
 --KERNEL
 ENGLISH_US["INSTALLP_DESC_NONPDRM"] =					"Allows you to bypass DRM protection on any PS Vita content"
 ENGLISH_US["INSTALLP_DESC_NONPDRMU"] =					"Allows you to bypass DRM protection on any PS Vita content(Unofficial MOD)"
@@ -291,6 +295,7 @@ ENGLISH_US["INSTALLP_DESC_LOWMEMMODE"] =				"Launch system applications with enl
 ENGLISH_US["INSTALLP_DESC_QUICKPOWER"] =				"Power off, reboot, and suspend, all from the quick menu. For both Playstation Vita and Playstation TV. Hold the Power Off button to reboot."
 ENGLISH_US["INSTALLP_DESC_QUICKVOLUME"] =				"Change volume from the quick menu on the Playstation Vita, just like on the Playstation TV."
 ENGLISH_US["INSTALLP_DESC_QASCREEN"] =				    "High quality screenshot plugin for PS Vita. Unlocks native QA screenshot SceShell implementation."
+ENGLISH_US["INSTALLP_DESC_NOSHAREDFB"] =				"Allows to bypass shared framebuffer issues after suspend-resume cycle for homebrew applications that run in system mode. but use standard framebuffer implementation."
 
 --ALL
 ENGLISH_US["INSTALLP_DESC_VSHMENU"] =					"A VSH menu taiHEN plugin for the PS VITA games and homebrews"
