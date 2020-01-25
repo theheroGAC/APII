a68b11c2d2f86a1d095c8a4fbeae084a0cea977b
ONELua Team <oneluaproject@gmail.com>
2020-01-25 08:43:51 -0600

    v0.04

--- Translations/Langdatabase.lua
+++ Translations/Langdatabase.lua
@@ -1,5 +1,5 @@
 Online_Langs = {
 { id =  "ENGLISH_US",	version = "01.04",		author =  "gdljjrod" },
-{ id =  "SPANISH",		version = "01.04",		author =  "gdljjrod" },
+{ id =  "SPANISH",		version = "01.05",		author =  "gdljjrod" },
 { id =  "ITALIAN",		version = "01.02",		author =  "TheHeroeGAC" },
 }
