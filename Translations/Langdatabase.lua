0c401535f65a3ccbd94be1c9443d976dcb38a48d
theheroGAC <gamesandconsolestaff@gmail.com>
2020-02-08 10:45:35 +0100

    Update Langdatabase.lua

--- Translations/Langdatabase.lua
+++ Translations/Langdatabase.lua
@@ -2,4 +2,5 @@ Online_Langs = {
 { id =  "ENGLISH_US",	version = "01.00",		author =  "gdljjrod" },
 { id =  "SPANISH",		version = "01.00",		author =  "gdljjrod" },
 { id =  "ITALIAN",		version = "01.00",		author =  "TheHeroeGAC" },
+{ id =  "JAPANESE",		version = "01.00",		author =  "Kouchan" },
 }
